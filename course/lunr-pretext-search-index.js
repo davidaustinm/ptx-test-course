ac1fb23e70232b3df838419339cd6086c35b2bba
davidaustinm <david.austin.m@gmail.com>
2026-05-07 16:08:15 +0000

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,149 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "syllabus",
+  "level": "1",
+  "url": "syllabus.html",
+  "type": "Section",
+  "number": "",
+  "title": "Syllabus",
+  "body": " Syllabus        Course Information  This is the syllabus for course name (MATH xxx, section xxx) for [term] 20xx. It is a [n] credit course.    Instructor  Prof. Lastname, Office Location, prof.lastname@example.edu .    Student Hours  TBD    Class meets  course times and location.    Course Description  course description from catalog    Prerequisite  list of prerequisites    Textbook and course materials   textbook name by textbook author.       Course Overview        Assessments and Grades     "
+},
+{
+  "id": "sec-course-info-2",
+  "level": "2",
+  "url": "syllabus.html#sec-course-info-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "course name (MATH xxx, section xxx) "
+},
+{
+  "id": "notes-week-01",
+  "level": "1",
+  "url": "notes-week-01.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 1",
+  "body": " Week 1   This is an outline of the topics we covered in the first week of class.     Monday 8\/22      Wednesday 8\/24      Friday 8\/26     "
+},
+{
+  "id": "notes-week-02",
+  "level": "1",
+  "url": "notes-week-02.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 2",
+  "body": " Week 2   Monday      Wednesday      Friday     "
+},
+{
+  "id": "activity-01-intro-activity",
+  "level": "1",
+  "url": "activity-01-intro-activity.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Introduction Activity",
+  "body": " Introduction Activity    This is the introduction to the activity.      This is the first exercise.    "
+},
+{
+  "id": "activity-01-intro-activity-3",
+  "level": "2",
+  "url": "activity-01-intro-activity.html#activity-01-intro-activity-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  This is the first exercise.   "
+},
+{
+  "id": "activities-4",
+  "level": "1",
+  "url": "activities-4.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Mathematics 204 Review",
+  "body": " Mathematics 204 Review    Suppose that     Write all the solution set of the equation in vector parametric form.    Remember that is the subspace that is the span of the columns of . Find a basis for and explain why your set is a basis.    Is the vector in ? If so, write it as a linear combination of basis vectors.    What is the dimension of and for which is a subspace of ?    Is the matrix invertible? Explain your response.    If a vector is in , what can you say about the set of solutions to the equation ?      We earlier defined the rank of a matrix to be the number of pivot positions in .    Give an example of a matrix for which . What does this say about the columns of ?    Give an example of a matrix whose entries are all nonzero for which .    Suppose that is a matrix. What is the largest possible value for ?    If is this largest possible value, what can you say about and what can you say about ?    If is a matrix, what is the largest possible value of ?    If is this largest possible value, what can you say about and what can you say about ?      Suppose that is a matrix and that and .    What is ?    Suppose that is a matrix and that What is ?    Find a solution to the equation .    What is the matrix ?      For what values of is the matrix invertible?      What does it mean for to be an eigenvector of a matrix ?    Suppose that is a matrix having eigenvectors and and eigenvalues and . Write the vector as a linear combination of eigenvectors.    Find the vector .    In the same way, write the vector as a linear combination of eigenvectors. Then find the vector .    What is the matrix ?    Is diagonalizable? If so, find the matrices and and verify that .    "
+},
+{
+  "id": "activities-4-2",
+  "level": "2",
+  "url": "activities-4.html#activities-4-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Suppose that     Write all the solution set of the equation in vector parametric form.    Remember that is the subspace that is the span of the columns of . Find a basis for and explain why your set is a basis.    Is the vector in ? If so, write it as a linear combination of basis vectors.    What is the dimension of and for which is a subspace of ?    Is the matrix invertible? Explain your response.    If a vector is in , what can you say about the set of solutions to the equation ?   "
+},
+{
+  "id": "activities-4-3",
+  "level": "2",
+  "url": "activities-4.html#activities-4-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  We earlier defined the rank of a matrix to be the number of pivot positions in .    Give an example of a matrix for which . What does this say about the columns of ?    Give an example of a matrix whose entries are all nonzero for which .    Suppose that is a matrix. What is the largest possible value for ?    If is this largest possible value, what can you say about and what can you say about ?    If is a matrix, what is the largest possible value of ?    If is this largest possible value, what can you say about and what can you say about ?   "
+},
+{
+  "id": "activities-4-4",
+  "level": "2",
+  "url": "activities-4.html#activities-4-4",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Suppose that is a matrix and that and .    What is ?    Suppose that is a matrix and that What is ?    Find a solution to the equation .    What is the matrix ?   "
+},
+{
+  "id": "activities-4-5",
+  "level": "2",
+  "url": "activities-4.html#activities-4-5",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  For what values of is the matrix invertible?   "
+},
+{
+  "id": "activities-4-6",
+  "level": "2",
+  "url": "activities-4.html#activities-4-6",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  What does it mean for to be an eigenvector of a matrix ?    Suppose that is a matrix having eigenvectors and and eigenvalues and . Write the vector as a linear combination of eigenvectors.    Find the vector .    In the same way, write the vector as a linear combination of eigenvectors. Then find the vector .    What is the matrix ?    Is diagonalizable? If so, find the matrices and and verify that .   "
+},
+{
+  "id": "handouts-2",
+  "level": "1",
+  "url": "handouts-2.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Handout Title",
+  "body": " Handout Title   A first paragraph with some space for notes below it.      v1=(2,1)  v2=(1,2)    {\\mathbf v}_1  {\\mathbf v}_2       A graph     f(x)=x^2-3            "
+},
+{
+  "id": "fig-prefig",
+  "level": "2",
+  "url": "handouts-2.html#fig-prefig",
+  "type": "Figure",
+  "number": "1",
+  "title": "",
+  "body": " A graph     f(x)=x^2-3         "
+},
+{
+  "id": "homework",
+  "level": "1",
+  "url": "homework.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Homework",
+  "body": " Homework    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
